43592ffdd88fd896a717babeec865286acb53c8e
cihy999 <cihy999@gmail.com>
2023-03-22 13:12:30 +0800

    feat: 新增 Saved Move Class

--- Source/Practice_UE4_CMC/Private/TestCharMovementCompoent.cpp
+++ Source/Practice_UE4_CMC/Private/TestCharMovementCompoent.cpp
@@ -2,6 +2,39 @@
 
 #include "TestCharMovementCompoent.h"
 
+bool UTestCharMovementCompoent::FSavedMove_Test::CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const
+{
+    // 如果資料相同，不需要送上server(=true)
+
+    FSavedMove_Test* pNewTestMove = static_cast<FSavedMove_Test*>(NewMove.Get());
+    if (Saved_bWantsToSprint != pNewTestMove->Saved_bWantsToSprint)
+    {
+        return false;
+    }
+
+    return Super::CanCombineWith(NewMove, InCharacter, MaxDelta);
+}
+
+void UTestCharMovementCompoent::FSavedMove_Test::Clear()
+{
+    Super::Clear();
+
+    Saved_bWantsToSprint = 0;
+}
+
+uint8 UTestCharMovementCompoent::FSavedMove_Test::GetCompressedFlags() const
+{
+    return uint8();
+}
+
+void UTestCharMovementCompoent::FSavedMove_Test::SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData)
+{
+}
+
+void UTestCharMovementCompoent::FSavedMove_Test::PrepMoveFor(ACharacter* C)
+{
+}
+
 UTestCharMovementCompoent::UTestCharMovementCompoent()
 {
 }
--- Source/Practice_UE4_CMC/Public/TestCharMovementCompoent.h
+++ Source/Practice_UE4_CMC/Public/TestCharMovementCompoent.h
@@ -14,6 +14,24 @@ class PRACTICE_UE4_CMC_API UTestCharMovementCompoent : public UCharacterMovement
 {
 	GENERATED_BODY()
 
+	class FSavedMove_Test : public FSavedMove_Character 
+	{
+		typedef FSavedMove_Character Super;
+
+		uint8 Saved_bWantsToSprint : 1;
+
+		/** Returns true if this move can be combined with NewMove for replication without changing any behavior */
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
+		/** Clear saved move properties, so it can be re-used. */
+		virtual void Clear() override;
+		/** Returns a byte containing encoded special movement information (jumping, crouching, etc.)	 */
+		virtual uint8 GetCompressedFlags() const override;
+		/** Called to set up this saved move (when initially created) to make a predictive correction. */
+		virtual void SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, class FNetworkPredictionData_Client_Character & ClientData) override;
+		/** Called before ClientUpdatePosition uses this SavedMove to make a predictive correction	 */
+		virtual void PrepMoveFor(ACharacter* C) override;
+	};
+
 	bool Safe_bWantsToSprint;
 
 public:
